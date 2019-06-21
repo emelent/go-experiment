8b42fd5a348ccf08e905f891a7d88464cdd920d0
Roy Motloutsi <roy@sticitt.co.za>
2019-06-21 11:11:55 +0200

    Create "misc" package

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,8 @@
 package main
 
 import "fmt"
+import misc "github.com/emelent/go-experiment/pkg/misc"
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println(misc.Greet("World"))
 }
---
+++ pkg/misc/misc.go
@@ -0,0 +1,8 @@
+package misc
+
+import "fmt"
+
+// Greet returns a string greeting a person by the name provided
+func Greet(name string) string {
+	return fmt.Sprintf("Hello there %s", name)
+}
